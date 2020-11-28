1540615cf07765fdf670820ae961d0097ae6b0c7
danm <d.v.maslennikov@outlook.com>
2020-11-28 22:23:35 +0300

    implement superior bot test/train/env

---
+++ SuperiorBotTest.py
@@ -0,0 +1,37 @@
+import time
+
+import numpy as np
+
+import halite_env
+from envs.superior_env import SuperiorEnv
+from models import LordTateKanti
+
+env = SuperiorEnv(
+    env=halite_env.Env(),
+    tiles_num=16,
+)
+env.configure(socket_path=f"/dev/shm/{time.time_ns()}", replay=True)
+
+model = LordTateKanti.make_model(env)
+print(model.summary())
+
+model.load_weights('dqn_SuperiorBot_weights_final.h5f')
+
+reward = 0
+observations = env.reset()
+while True:
+    # print(f'{observations=}')
+    action = np.argmax(model.predict(observations.reshape((1, -1))))
+
+    # print(f'{actions=}')
+    print(f'{action=}')
+
+    observations, reward, done, _ = env.step(action)
+
+    print(f'{reward=}')
+
+    if done:
+        break
+
+env.close()
+print("done")
--- envs/superior_env.py
+++ envs/superior_env.py
@@ -1,26 +1,29 @@
-import numpy as np
 import time
-from envs.grid import Grid
-from gym.spaces import Discrete
 from math import sqrt, floor
 
+import numpy as np
+from gym.spaces import Discrete
+
 import hlt
-from halite_env import navigate
+from envs.grid import Grid
+from halite_env import navigate, attack
+from hlt.entity import Position
 from hlt.game_map import Map
 
 
 class SuperiorEnv:
-    def __init__(self, env, squares_num, feature_len):
+    def __init__(self, env, tiles_num):
         self.env = env
-        self.tiles_num = squares_num
-        self.action_space = Discrete(squares_num)
-        self.observation_space = np.zeros((squares_num, feature_len))
+        self.tiles_num = tiles_num
+        self.FEATURE_LEN = 10
+        self.action_space = Discrete(tiles_num)
+        self.observation_space = np.zeros((tiles_num, self.FEATURE_LEN))
         self.map = None
         self.grid = None
         self.start_round = 0
-        self.FEATURE_LEN = feature_len
 
     def step(self, action):
+        # print(f'{action=}')
         commands = self._get_commands(action, self.map)
         self.map, _, done, _ = self.env.step(commands)
         self.start_round = time.time()
@@ -33,7 +36,7 @@ class SuperiorEnv:
     def reset(self):
         self.map: Map = self.env.reset()
         self.grid: Grid = Grid(self.tiles_num, self.map.height, self.map.width)
-
+        # print(self.grid)
         return self._get_observations(self.map)
 
     def close(self):
@@ -49,10 +52,13 @@ class SuperiorEnv:
         commands = []
         player_id = world_map.get_me().id
         planets = world_map.all_planets()
-        ships = world_map.get_me().all_ships()
+        all_ships = world_map._all_ships()
+        ships = [ship for ship in all_ships if ship.owner.id == player_id]
+        enemy_ships = [ship for ship in world_map._all_ships() if ship.owner.id != player_id]
 
         free_planets = []
         enemy_planets = []
+        enemy_ships_on_tile = []
         target_tile = floor(action)
         for planet in planets:
             planet_tile = self.grid.get_tile_id(planet.x, planet.y)
@@ -61,6 +67,13 @@ class SuperiorEnv:
             elif planet_tile == target_tile and planet.owner.id != player_id:
                 enemy_planets.append(planet)
 
+        tile_center_x, tile_center_y = self.grid.get_tile_center_by_id(target_tile)
+
+        for ship in enemy_ships:
+            ship_tile = self.grid.get_tile_id(ship.x, ship.y)
+            if ship_tile == target_tile:
+                enemy_ships_on_tile.append(ship)
+
         if len(free_planets) > 0:
             for ship in ships:
                 dest_planet = free_planets[np.argmin([
@@ -68,7 +81,7 @@ class SuperiorEnv:
                     for planet in free_planets
                 ])]
                 if ship.can_dock(dest_planet):
-                    ship.dock(dest_planet)
+                    commands.append(ship.dock(dest_planet))
                 else:
                     commands.append(navigate(
                         world_map,
@@ -76,6 +89,18 @@ class SuperiorEnv:
                         ship,
                         ship.closest_point_to(dest_planet),
                         speed=int(hlt.constants.MAX_SPEED / 2)))
+        elif len(enemy_ships_on_tile) > 0:
+            for ship in ships:
+                dest_ship = enemy_ships_on_tile[np.argmin([
+                    np.linalg.norm((enemy.x - ship.x, enemy.y - ship.y))
+                    for enemy in enemy_ships_on_tile
+                ])]
+                commands.append(attack(
+                    world_map,
+                    self.start_round,
+                    ship,
+                    Position(dest_ship.x, dest_ship.y),
+                    speed=int(hlt.constants.MAX_SPEED / 2)))
         elif len(enemy_planets) > 0:
             for ship in ships:
                 dest_planet = enemy_planets[np.argmin([
@@ -83,7 +108,7 @@ class SuperiorEnv:
                     for planet in enemy_planets
                 ])]
                 if ship.can_dock(dest_planet):
-                    ship.dock(dest_planet)
+                    commands.append(ship.dock(dest_planet))
                 else:
                     commands.append(navigate(
                         world_map,
@@ -92,26 +117,28 @@ class SuperiorEnv:
                         ship.closest_point_to(dest_planet),
                         speed=int(hlt.constants.MAX_SPEED / 2)))
         else:
-            tile_center = self.grid.get_tile_center_by_id(target_tile)
             for ship in ships:
                 commands.append(navigate(
                     world_map,
                     self.start_round,
                     ship,
-                    tile_center,
+                    Position(tile_center_x, tile_center_y),
                     speed=int(hlt.constants.MAX_SPEED / 2)))
 
         return commands
 
     def _calc_reward(self, map: hlt.game_map.Map):
         player_id = map.get_me().id
+        planets = map.all_planets()
         my_planets = 0
-        for planet in map.all_planets():
+        total_planets = len(planets)
+        for planet in planets:
             if planet.is_owned() and planet.owner.id == player_id:
                 my_planets += 1
-        return my_planets / len(map.all_planets())
+        return (my_planets / total_planets)**2
 
-    # Общее здоровье планет в тайле / общее здоровье
+        # Общее здоровье планет в тайле / общее здоровье
+
     # Общее количество союзных пришвартованных кораблей /  максимальное количество пришвартованных кораблей в этом
     # кводрате
     # Общее количество враждебных пришвартованных кораблей / максимальное количество пришвартованных кораблей в этом
@@ -138,23 +165,15 @@ class SuperiorEnv:
         total_ships_number = len(all_ships)
         total_planets = len(planets)
 
-        my_distances = np.zeros(self.tiles_num)
-        enemy_distances = np.zeros(self.tiles_num)
+        my_distances = np.full(self.tiles_num, np.inf)
+        enemy_distances = np.full(self.tiles_num, np.inf)
 
         for ship in all_ships:
-            ship_docked = ship.planet is not None
-            ship_mine = ship.owner == player_id
-            if ship_docked and ship_mine:
-                enemy_docked, my_docked = 0, int(ship_mine)
-            elif ship_docked:
-                enemy_docked, my_docked = int(ship_mine), 0
-            else:
-                enemy_docked, my_docked = 0, 0
-
+            ship_mine = ship.owner.id == player_id
             features = [
                 0,
-                my_docked,
-                enemy_docked,
+                0,
+                0,
                 0,
                 0,
                 int(ship_mine),
@@ -205,13 +224,16 @@ class SuperiorEnv:
                 int(not owned),
                 1
             ]
+
             observation[tile_id] += np.array(features)
 
         for i in range(self.tiles_num):
             tile_features = observation[i]
             tile_features[0] /= total_planets_health
-            tile_features[1] /= total_docking_spots[i]
-            tile_features[2] /= total_docking_spots[2]
+            docking_spots = total_docking_spots[i]
+            if docking_spots != 0:
+                tile_features[1] /= total_docking_spots[i]
+                tile_features[2] /= total_docking_spots[i]
             tile_features[3] = my_distances[i] / diagonal
             tile_features[4] = enemy_distances[i] / diagonal
             tile_features[5] /= total_ships_number
@@ -219,5 +241,5 @@ class SuperiorEnv:
             tile_features[7] /= total_planets
             tile_features[8] /= total_planets
             tile_features[9] /= total_planets
-
+        # print(f'{observation=}')
         return observation
